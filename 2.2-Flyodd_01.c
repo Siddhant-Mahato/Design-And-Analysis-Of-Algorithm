3443c2f4887f8eb157ebb7f083b8aa04cdaae00a
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-08-11 00:07:42 +0530

    Flyodd_01

---
+++ 2.2-Flyodd_01.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define V 4
+
+void printSolution(int dist[][V])
+{
+    printf("Shortest distances between every pair of vertices:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INT_MAX) {
+                printf("INF ");
+            }
+            else {
+                printf("%d ", dist[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+void floydWarshall(int graph[][V])
+{
+    int dist[V][V];
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && 
+                    dist[i][j] > dist[i][k] + dist[k][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    printSolution(dist);
+}
+
+int main()
+{
+    int graph[V][V] = {{0, 3, INT_MAX, 5},
+                       {2, 0, INT_MAX,4},
+                       {INT_MAX, 1, 0, INT_MAX},
+                       {INT_MAX, INT_MAX, 2, 0}};
+
+    floydWarshall(graph);
+
+    return 0;
+}
+
